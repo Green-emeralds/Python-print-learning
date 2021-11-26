20169f0c64a6f71ebc1b9dc8ac066d4b4ff6b9f0
fredp <fredp@DESKTOP-SFN3LND>
2021-11-26 19:43:36 +0000

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,6 @@
+food_name = "turds"
+character_name = "Tim Bristow"
+print("There was a man on a bus named " + character_name + ",")
+print("He liked " + food_name + " alot")
+print("So he robbed a " + food_name + " store and got caught")
+print("So when he was in jail he dropped the soap on purpose")
